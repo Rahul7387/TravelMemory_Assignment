fb92567ff437c1d0a8110555733cd4a18afba692
Rahul7387 <rahulgupta7387@gmail.com>
2026-07-05 22:19:41 +0530

    UPDATE: add MongoDB connection success log and clean up conn.js

--- backend/conn.js
+++ backend/conn.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
 const URL = process.env.MONGO_URI
-
 mongoose.connect(URL)
 mongoose.Promise = global.Promise
-
 const db = mongoose.connection
+db.once('open', () => console.log('MongoDB connected successfully!'))
 db.on('error', console.error.bind(console, 'DB ERROR: '))
-
 module.exports = {db, mongoose}
